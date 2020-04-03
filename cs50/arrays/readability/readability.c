126d4511e816b46d9a1534de59572ee33b991869
hauze-v <hauze_v@protonmail.com>
2020-04-03 08:04:31 -0400

    working word function

--- cs50/arrays/readability/readability.c
+++ cs50/arrays/readability/readability.c
@@ -1,25 +1,30 @@
 # include <cs50.h>
 # include <stdio.h>
 # include <string.h>
+# include <ctype.h>
 
 int main(void) {
 
     string text = get_string("Text: ");
 
-    int letters = 0;
-
     // Count letters
+    int letters = 0;
     for (int i = 0; i < strlen(text); i++) {
-
-        // Check if letter (ASCII)
-        if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+        // Check if letter and add if so
+        if (isalpha(text[i]))
         {
             letters++;
         }
     }
 
     // Count words
-    int words = 0;
-
-    printf("%i\n", letters);
+    int words = 1;
+    for (int i=0; i < strlen(text); i++) {
+        if (text[i-1] == 32 && isalpha(text[i])){
+            words++;
+        }
+    }
+    
+    printf("Letters: %i\n", letters);
+    printf("Word: %i\n", words);
 }
