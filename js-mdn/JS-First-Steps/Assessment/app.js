5edd3fc34cb3c4d883467372d28236d13705e8e3
Adam Hauze <hauze_v@protonmail.com>
2020-07-09 10:05:34 -0400

    Working ona  silly story generator app

---
+++ js-mdn/JS-First-Steps/Assessment/app.js
@@ -0,0 +1,40 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+
+/* STORY VARIABLES */
+// let protagonist = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+// let location = ["the soup kitchen", "Disneyworld", "the White House"];
+// let crisis = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+// const storyText = `It was 94 fahrenheit outside, so ${randomValueFromArray(protagonist)} went for a walk. When they got to ${randomValueFromArray(location)}, they stared in horror for a few moments, then ${randomValueFromArray(crisis)}. Bob saw the whole thing, but was not surprised - ${randomValueFromArray(protagonist)} weighs 300 pounds, and it was a hot day.`
+// console.log(storyText);
+
+
+/* 3. EVENT LISTENER AND FUNCTION DEFINITION */
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  let name = "Bob"
+
+  if (customName.value !== '') {
+    name = customName.value;
+  }
+
+  if (document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature = Math.round(94);
+
+  }
+
+  story.textContent = storyText;
+  story.style.visibility = 'visible';
+}
